05b481da33d5116a84be3c63f379c507b0a15732
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-03-29 12:48:42 +0200

    Added functionality to the Add button in text-bill.js file

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,18 +1,25 @@
+
+//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
+// * add the appropriate value to the running total
+// * add nothing for invalid values that is not 'call' or 'sms'.
+// * display the latest total on the screen
+
+
 var callsTotal = 0;
 var smsTotal = 0;
 
 // get a reference to the textbox where the bill type is to be entered
-var textbox = document.querySelector(".billTypeText");
+var billTypeText = document.querySelector(".billTypeText");
 
 //get a reference to the add button
 var textTotalAddBtn = document.querySelector(".addToBillBtn");
 
 //create a variable that will keep track of the total bill
-var totalBill = document.querySelector(".totalOne");
+var totalCostElem = document.querySelector(".totalOne");
 
-
-var callBill = document.querySelector(".callTotalOne");
-var smsBill = document.querySelector(".smsTotalOne");
+// Variables that will track the total of calls and sms
+var callsTotalElem = document.querySelector(".callTotalOne");
+var smsTotalElem = document.querySelector(".smsTotalOne");
 
 
 
@@ -22,7 +29,7 @@ function textBillTotal(){
     // get the value entered in the billType textfield
     var billTypeEntered = billTypeText.value.trim();
     // update the correct total
-    if (billTypeEntered === "bill"){
+    if (billTypeEntered === "call"){
         callsTotal += 2.75
     }
     else if (billTypeEntered === "sms"){
@@ -34,19 +41,9 @@ function textBillTotal(){
     smsTotalElem.innerHTML = smsTotal.toFixed(2);
     var totalCost = callsTotal + smsTotal;
     totalCostElem.innerHTML = totalCost.toFixed(2);
-}
-
-
-function textBillTotal(){
-
-    // ... other code here
     
-    //update the totals that is displayed on the screen.
-    callsTotalElem.innerHTML = callsTotal.toFixed(2);
-    smsTotalElem.innerHTML = smsTotal.toFixed(2);
-    var totalCost = callsTotal + smsTotal;
-    totalCostElem.innerHTML = totalCost.toFixed(2);
     
+      
     //color the total based on the criteria
     if (totalCost >= 50){
         // adding the danger class will make the text red
@@ -56,10 +53,7 @@ function textBillTotal(){
         totalCostElem.classList.add("warning");
     }
 }
-
 textTotalAddBtn.addEventListener('click', textBillTotal);
 
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+
+
